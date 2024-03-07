a3cb6c6aedef935f5b89324cf769eb973bb7e4f6
knm22mukk <knm22mukk@gmail.com>
2024-03-07 09:22:30 +0900

    Buttonにhoverアクションを追加しました

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,7 +7,7 @@ export function Button({ buttonType, buttonText }: Props) {
   return (
     <button
       type={buttonType}
-      className='rounded bg-orange-600 px-8 py-3 font-bold text-gray-100'
+      className='rounded bg-orange-500 px-8 py-3 text-lg font-bold text-gray-100 hover:bg-orange-600'
     >
       {buttonText}
     </button>
@@ -18,7 +18,7 @@ export function BorderButton({ buttonType, buttonText }: Props) {
   return (
     <button
       type={buttonType}
-      className='rounded border border-orange-600 px-8 py-3 font-bold text-orange-600'
+      className='rounded border border-orange-500 px-8 py-3 text-lg font-bold text-orange-500 hover:bg-orange-600 hover:text-white'
     >
       {buttonText}
     </button>
