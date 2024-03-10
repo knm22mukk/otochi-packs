666ce66bc74b940797823ffacd4ae7a718f58624
knm22mukk <knm22mukk@gmail.com>
2024-03-10 09:28:10 +0900

    初回ロード時ののテーマを取得できるように修正しました

--- src/components/ThemeToggleButton.tsx
+++ src/components/ThemeToggleButton.tsx
@@ -1,18 +1,31 @@
 'use client';
 
 import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
 import { MdOutlineWbSunny, MdOutlineNightlight } from 'react-icons/md';
 
 export default function ThemeToggleButton() {
   const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
   const handleClick = () => {
     setTheme(theme === 'dark' ? 'light' : 'dark');
   };
 
   return (
-    <button type='button' aria-label='Theme Toggle Button' onClick={handleClick}>
-      {theme === 'light' ? <MdOutlineNightlight /> : <MdOutlineWbSunny />}
+    <button
+      type='button'
+      aria-label='Theme Toggle Button'
+      onClick={handleClick}
+      className='h-10 w-10 rounded p-2 text-xl font-bold text-orange-500'
+    >
+      {mounted && (
+        <>{theme === 'light' ? <MdOutlineNightlight /> : <MdOutlineWbSunny />}</>
+      )}
     </button>
   );
 }
